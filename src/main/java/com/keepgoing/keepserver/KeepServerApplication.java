e5c3e9576641b8e70bbe28143bfbdd247336f22f
최미래 <miraechoi@dgsw.hs.kr>
2024-03-28 21:26:30 +0900

    Initial commit

---
+++ src/main/java/com/keepgoing/keepserver/KeepServerApplication.java
@@ -0,0 +1,13 @@
+package com.keepgoing.keepserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keepgoing/keepserver/KeepServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepgoing.keepserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
