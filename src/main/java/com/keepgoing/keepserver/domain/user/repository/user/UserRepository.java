9d9ba60d836d8e551656b01f75dcf0729a4d19d0
최미래 <miraechoi@dgsw.hs.kr>
2024-04-11 20:04:01 +0900

    Feat :: create repositories
    role, user, userroles

---
+++ src/main/java/com/keepgoing/keepserver/domain/user/repository/user/UserRepository.java
@@ -0,0 +1,16 @@
+package com.keepgoing.keepserver.domain.user.repository.user;
+
+import com.keepgoing.keepserver.domain.user.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByEmailEquals(String email);
+}
