6e5e150f027d3230cd33e2345176c90da1757f5c
최미래 <miraechoi@dgsw.hs.kr>
2024-04-11 20:13:21 +0900

    Feat :: create security-service files

---
+++ src/main/java/com/keepgoing/keepserver/domain/user/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.keepgoing.keepserver.domain.user.security.service;
+
+import com.keepgoing.keepserver.domain.user.entity.user.User;
+import com.keepgoing.keepserver.domain.user.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일을 찾을 수 없습니다: " + email));
+        return UserDetailsImpl.build(userEntity);
+    }
+}
