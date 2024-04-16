bccdd08836fa8ac51431fddf1f2af54a70f6ccf1
priverg163 <priverg163@dgsw.hs.kr>
2024-04-16 20:54:16 +0900

    Feat :: bulider & dto

---
+++ src/main/java/com/keepgoing/keepserver/domain/book/domain/BookDTO.java
@@ -0,0 +1,20 @@
+package com.keepgoing.keepserver.domain.book.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+public class BookDTO {
+    private long id;
+    private String name;
+    private String nfcCode;
+    private String writer;
+    private Date registrationDate;
+    private String state;
+
+
+
+}
