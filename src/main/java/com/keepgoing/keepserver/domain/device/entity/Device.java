26e59a010ee6098bfe6389db2d51d1b74765aeb3
최미래 <miraechoi@dgsw.hs.kr>
2024-05-04 15:22:21 +0900

    Feat :: create Device

---
+++ src/main/java/com/keepgoing/keepserver/domain/device/entity/Device.java
@@ -0,0 +1,47 @@
+package com.keepgoing.keepserver.domain.device.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "device")
+public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /*
+        기기 이름
+    */
+    @Column(nullable = false)
+    private String deviceName;
+
+    /*
+        기기 사진
+    */
+    @Column(nullable = false)
+    private String imgUrl;
+
+    /*
+        기기 대여 상태
+    */
+    @Column(nullable = false)
+    private int status;  /* 1 대여 가능, 2 대여 불가능 */
+
+    @Column(nullable = false)
+    private Long check_info; // 비디오 작성자 pk 저장용
+
+    @Builder
+    public Device(String deviceName, int status, String imgUrl, Long check_info) {
+        this.deviceName = deviceName;
+        this.status = status;
+        this.imgUrl = imgUrl;
+        this.check_info = check_info;
+    }
+}
