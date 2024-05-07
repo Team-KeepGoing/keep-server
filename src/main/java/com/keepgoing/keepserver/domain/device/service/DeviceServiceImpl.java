f89c619b794bbbb5cc24e208090c84a3813ed4e2
최미래 <miraechoi@dgsw.hs.kr>
2024-05-07 16:25:57 +0900

    Feat :: create deleteDevice logic

--- src/main/java/com/keepgoing/keepserver/domain/device/service/DeviceServiceImpl.java
+++ src/main/java/com/keepgoing/keepserver/domain/device/service/DeviceServiceImpl.java
@@ -5,6 +5,7 @@ import com.keepgoing.keepserver.domain.device.payload.response.DeviceResponseDto
 import com.keepgoing.keepserver.domain.device.repository.DeviceRepository;
 import com.keepgoing.keepserver.domain.user.repository.user.UserRepository;
 import com.keepgoing.keepserver.global.dto.response.BaseResponse;
+import com.keepgoing.keepserver.global.exception.device.DeviceError;
 import com.keepgoing.keepserver.global.exception.device.DeviceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -47,8 +48,17 @@ public class DeviceServiceImpl implements DeviceService {
     }
 
     @Override
-    public ResponseEntity<BaseResponse> deleteDevice(Long id, Authentication authentication) {
-        return null;
+    public BaseResponse deleteDevice(Long id, Authentication authentication) {
+        Device device = deviceRepository.findById(id).orElseThrow(DeviceException::notFoundDevice);
+        String info = device.getCheck_info().toString();
+
+        if (info.equals(authentication.getName())){
+            deviceRepository.deleteById(id);
+        } else {
+            throw new DeviceException(DeviceError.DEVICE_NOT_FOUND_EXCEPTION);
+        }
+
+        return new BaseResponse(HttpStatus.OK, "기기 삭제 성공");
     }
 
     @Override
