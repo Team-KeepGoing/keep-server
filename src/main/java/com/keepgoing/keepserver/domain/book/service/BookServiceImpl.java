59ab3df07d9fa7dc40426ff3830a50cd6b35f733
priverg163 <priverg163@dgsw.hs.kr>
2024-05-08 20:53:29 +0900

    Remove :: DirtyChecking

--- src/main/java/com/keepgoing/keepserver/domain/book/service/BookServiceImpl.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/service/BookServiceImpl.java
@@ -3,15 +3,11 @@ package com.keepgoing.keepserver.domain.book.service;
 import com.keepgoing.keepserver.domain.book.entity.Book;
 import com.keepgoing.keepserver.domain.book.repository.BookRepository;
 import com.keepgoing.keepserver.domain.book.util.GenerateCertCharacter;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -36,39 +32,23 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public void deleteBookByNfcCode(String nfcCode) {
-        bookRepository.deleteAllByNfcCode(nfcCode);
+        bookRepository.deleteByNfcCode(nfcCode);
     }
+
 
     @Override
     public String createNfcCode() {
         GenerateCertCharacter generateCertCharacter = new GenerateCertCharacter();
         return generateCertCharacter.excuteGenerate();
     }
+//
+//    @Override
+//    public void editBookByNfcCode(String nfcCode) {
+//
+//    }
 
-    @Override
-    public void editBookByNfcCode(String nfcCode) {
-
-    }
 }
 
-    class DirtyChecking {
-
-        BookRepository bookRepository;
-
-
-        @Transactional
-        public void updateBook(String nfcCode, String name) {
-
-            // DB에서 id값을 기준으로 데이터를 찾는다 (영속화)
-            List<Book> book = bookRepository.findByNfcCode(nfcCode);
-
-
-        }
-    }
-
-
-
-//}
 
 
 
