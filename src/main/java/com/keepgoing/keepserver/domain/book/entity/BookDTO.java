6e9a6d20241aec22f55c6fecad333a09472e4852
priverg163 <priverg163@dgsw.hs.kr>
2024-05-09 17:48:43 +0900

    Feat :: DeleteBookByNfcCode

--- src/main/java/com/keepgoing/keepserver/domain/book/entity/BookDTO.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/entity/BookDTO.java
@@ -4,10 +4,12 @@ import com.keepgoing.keepserver.domain.book.util.GenerateCertCharacter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class BookDTO {
     GenerateCertCharacter generateCertCharacter = new GenerateCertCharacter();
