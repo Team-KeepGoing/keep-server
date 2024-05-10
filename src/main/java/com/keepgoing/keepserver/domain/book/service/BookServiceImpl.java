24a6ce46065c8bba18161170ac48240d6475a8b3
priverg163 <priverg163@dgsw.hs.kr>
2024-05-10 11:50:03 +0900

    Edit :: bookRegistration

--- src/main/java/com/keepgoing/keepserver/domain/book/service/BookServiceImpl.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/service/BookServiceImpl.java
@@ -1,13 +1,18 @@
 package com.keepgoing.keepserver.domain.book.service;
 
 import com.keepgoing.keepserver.domain.book.entity.Book;
+import com.keepgoing.keepserver.domain.book.entity.BookDTO;
 import com.keepgoing.keepserver.domain.book.repository.BookRepository;
 import com.keepgoing.keepserver.domain.book.util.GenerateCertCharacter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -15,13 +20,18 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
     @Override
-    public void bookRegistration(Book book) {
+    public String bookRegistration(Book book, MultipartFile multipartFile) {
+
         String nfcCode = createNfcCode();
         book.setRegistrationDate(new Date());
         book.setNfcCode(nfcCode);
         book.setState("N");
         bookRepository.save(book);
+        return "success";
+
     }
+
+
 
     @Override
     public ArrayList<Book> selectAllBook() {
@@ -44,6 +54,7 @@ public class BookServiceImpl implements BookService {
         GenerateCertCharacter generateCertCharacter = new GenerateCertCharacter();
         return generateCertCharacter.excuteGenerate();
     }
+
 
 }
 
