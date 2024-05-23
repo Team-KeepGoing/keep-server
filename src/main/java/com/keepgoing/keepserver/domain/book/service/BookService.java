26d7e7a0181042499088b9c685e5fa180931da46
priverg163 <priverg163@dgsw.hs.kr>
2024-05-23 18:05:00 +0900

    Refactor :: Removed unnecessary comments from BookService class

--- src/main/java/com/keepgoing/keepserver/domain/book/service/BookService.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/service/BookService.java
@@ -1,22 +1,21 @@
 package com.keepgoing.keepserver.domain.book.service;
 
 import com.keepgoing.keepserver.domain.book.entity.Book;
+import com.keepgoing.keepserver.domain.book.repository.dto.BookRequestDTO;
 import com.keepgoing.keepserver.global.common.BaseResponse;
+import jakarta.transaction.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface BookService {
-//    public void bookRegistration2(Book book, MultipartFile multipartFile);
-//    public List<Book> selectAllBook2();
-//    public String deleteBookByNfcCode2(String nfcCode);
-//    public String createNfcCode2();
-
     BaseResponse bookRegister(Book book, MultipartFile multipartFile);
     BaseResponse selectAllBook();
     BaseResponse deleteBook(String nfcCode);
     public String createNfcCode();
-
+    @Transactional
+    BaseResponse editBook(String nfcCode, BookRequestDTO bookRequest) throws IOException;
 }
 
 
