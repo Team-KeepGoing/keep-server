9e05991525a5696b90f03c5090ef1025374999ea
최미래 <miraechoi@dgsw.hs.kr>
2024-05-25 23:10:58 +0900

    Remove :: remove unused annotation

--- src/main/java/com/keepgoing/keepserver/domain/device/payload/response/MyDevicesDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/device/payload/response/MyDevicesDto.java
@@ -1,10 +1,8 @@
 package com.keepgoing.keepserver.domain.device.payload.response;
 
 import lombok.Builder;
-import lombok.Data;
 
 @Builder
-@Data
 public record MyDevicesDto (
         Long id,
         String deviceName ) {
