732bf377480864d8dbec0a3a1012ca29a914d0ad
priverg163 <priverg163@dgsw.hs.kr>
2024-06-07 23:31:25 +0900

    Edit :: ExcelController

--- src/main/java/com/keepgoing/keepserver/domain/excel/controller/ExcelController.java
+++ src/main/java/com/keepgoing/keepserver/domain/excel/controller/ExcelController.java
@@ -1,7 +1,23 @@
 package com.keepgoing.keepserver.domain.excel.controller;
 
-import org.springframework.stereotype.Controller;
+import com.keepgoing.keepserver.domain.excel.service.ExcelService;
+import com.keepgoing.keepserver.global.common.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/excel")
 public class ExcelController {
+    private final ExcelService excelService;
+    @PostMapping("/upload")
+    public BaseResponse uploadExcel(@RequestPart("file") MultipartFile file) throws IOException {
+        return excelService.uploadExcel(file);
+    }
 }
--- src/main/java/com/keepgoing/keepserver/domain/excel/dto/ExcelDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/excel/dto/ExcelDto.java
@@ -1,4 +1,10 @@
 package com.keepgoing.keepserver.domain.excel.dto;
 
 public class ExcelDto {
+    String name;
+    int grade;
+    int group;
+    int groupNum;
+    int phoneNum;
+    String address;
 }
