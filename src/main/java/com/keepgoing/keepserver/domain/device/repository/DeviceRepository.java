20985689554ecc9be56b1ebc082361a446ee7494
최미래 <miraechoi@dgsw.hs.kr>
2024-06-08 11:44:14 +0900

    Remove :: remove unused findByDeviceNameContaining

--- src/main/java/com/keepgoing/keepserver/domain/device/repository/DeviceRepository.java
+++ src/main/java/com/keepgoing/keepserver/domain/device/repository/DeviceRepository.java
@@ -1,14 +1,10 @@
 package com.keepgoing.keepserver.domain.device.repository;
 
 import com.keepgoing.keepserver.domain.device.entity.Device;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
-    List<Device> findByDeviceNameContaining(String deviceName, Sort id);
-
     Optional<Device> findByDeviceName(String deviceName);
 }
