531f0a209484c3c9839348bf85599e85b4ed3470
최미래 <miraechoi@dgsw.hs.kr>
2024-06-08 12:37:28 +0900

    Feat :: add provideUserInfo to UserService

--- src/main/java/com/keepgoing/keepserver/domain/user/service/user/UserService.java
+++ src/main/java/com/keepgoing/keepserver/domain/user/service/user/UserService.java
@@ -1,8 +1,12 @@
 package com.keepgoing.keepserver.domain.user.service.user;
 
+import com.keepgoing.keepserver.domain.user.payload.request.UserInfoRequest;
+import com.keepgoing.keepserver.domain.user.payload.request.UserProfileDto;
 import com.keepgoing.keepserver.global.exception.BusinessException;
 import com.keepgoing.keepserver.domain.user.payload.request.SignupRequest;
 
 public interface UserService {
     void registerUser(SignupRequest signupRequest) throws BusinessException;
+    void updateUserData(UserInfoRequest request, String email);
+    UserProfileDto provideUserInfo(String userEmail);
 }
