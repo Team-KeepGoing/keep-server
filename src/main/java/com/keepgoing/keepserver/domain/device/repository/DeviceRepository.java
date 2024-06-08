9e5b972cfd06e728761c7c84c73fccb78ee8cd16
최미래 <miraechoi@dgsw.hs.kr>
2024-06-08 11:45:48 +0900

    Feat :: add findByBorrower

--- src/main/java/com/keepgoing/keepserver/domain/device/repository/DeviceRepository.java
+++ src/main/java/com/keepgoing/keepserver/domain/device/repository/DeviceRepository.java
@@ -1,10 +1,13 @@
 package com.keepgoing.keepserver.domain.device.repository;
 
 import com.keepgoing.keepserver.domain.device.entity.Device;
+import com.keepgoing.keepserver.domain.user.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
     Optional<Device> findByDeviceName(String deviceName);
+    List<Device> findByBorrower(User borrower);
 }
