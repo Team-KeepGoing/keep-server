ca933013cde0f3579a7b8d5963129d00681b4821
priverg163 <priverg163@dgsw.hs.kr>
2024-06-08 22:52:22 +0900

    Edit :: excel service

--- src/main/java/com/keepgoing/keepserver/domain/excel/controller/ExcelController.java
+++ src/main/java/com/keepgoing/keepserver/domain/excel/controller/ExcelController.java
@@ -3,10 +3,7 @@ package com.keepgoing.keepserver.domain.excel.controller;
 import com.keepgoing.keepserver.domain.excel.service.ExcelService;
 import com.keepgoing.keepserver.global.common.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,7 +14,7 @@ import java.io.IOException;
 public class ExcelController {
     private final ExcelService excelService;
     @PostMapping("/upload")
-    public BaseResponse uploadExcel(@RequestPart("file") MultipartFile file) throws IOException {
+    public BaseResponse uploadExcel(@RequestParam("file") MultipartFile file) throws IOException {
         return excelService.uploadExcel(file);
     }
 }
--- src/main/java/com/keepgoing/keepserver/domain/excel/dto/ExcelDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/excel/dto/ExcelDto.java
@@ -1,10 +1,10 @@
 package com.keepgoing.keepserver.domain.excel.dto;
 
 public class ExcelDto {
-    String name;
-    int grade;
-    int group;
-    int groupNum;
-    int phoneNum;
-    String address;
+    private String name;
+    private int grade;
+    private int group;
+    private int groupNum;
+    private int phoneNum;
+    private String address;
 }
