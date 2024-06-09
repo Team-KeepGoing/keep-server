316c64471005ac9976379f0a76e2adf2adfec10b
최미래 <miraechoi@dgsw.hs.kr>
2024-06-09 22:18:33 +0900

    Refactor :: code format #36

--- src/main/java/com/keepgoing/keepserver/domain/device/payload/response/DeviceResponseDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/device/payload/response/DeviceResponseDto.java
@@ -3,9 +3,10 @@ package com.keepgoing.keepserver.domain.device.payload.response;
 import lombok.Builder;
 
 @Builder
-public record DeviceResponseDto(
+public record DeviceResponseDto (
         Long id,
         String deviceName,
         String imgUrl,
-        boolean status) {
+        boolean status
+) {
 }
