94660f790dadd77620d10992d21089fab0cb4a90
최미래 <miraechoi@dgsw.hs.kr>
2024-06-09 23:19:55 +0900

    Refactor :: refactor device error #36

--- src/main/java/com/keepgoing/keepserver/global/exception/device/DeviceException.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/device/DeviceException.java
@@ -6,7 +6,7 @@ public class DeviceException extends BusinessException {
     private static final DeviceException USER_NOT_FOUND = new DeviceException(DeviceError.USER_NOT_FOUND);
 
     private static final DeviceException DEVICE_NOT_FOUND_EXCEPTION = new DeviceException(DeviceError.DEVICE_NOT_FOUND_EXCEPTION);
-    private static final DeviceException DEVICE_ALREADY_RENTED = new DeviceException(DeviceError.DEVICE_ALREADY_RENTED);
+    private static final DeviceException DEVICE_NOT_AVAILABLE = new DeviceException(DeviceError.DEVICE_NOT_AVAILABLE);
 
     public DeviceException(DeviceError error) {
         super(error);
@@ -19,7 +19,7 @@ public class DeviceException extends BusinessException {
     public static DeviceException notFoundDevice() {
         return DEVICE_NOT_FOUND_EXCEPTION;
     }
-    public static DeviceException deviceAlreadyRented() {
-        return DEVICE_ALREADY_RENTED;
+    public static DeviceException deviceNotAvailable() {
+        return DEVICE_NOT_AVAILABLE;
     }
 }
