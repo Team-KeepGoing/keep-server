644c5894693d1b39f5531f4a18cf99473c19abac
최미래 <miraechoi@dgsw.hs.kr>
2024-06-10 18:15:40 +0900

    Add :: add findByBookName

--- src/main/java/com/keepgoing/keepserver/domain/book/repository/BookRepository.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/repository/BookRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookByNfcCode(String NfcCode);
-    List<Book> findByNameContaining(String name);
+    List<Book> findByBookNameContaining(String bookName);
+    Optional<Book> findByBookName(String bookName);
 }
