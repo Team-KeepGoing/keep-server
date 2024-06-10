a5d10fc12d8ec0302e4a458ff5c43020479fe3c8
최미래 <miraechoi@dgsw.hs.kr>
2024-06-10 18:17:19 +0900

    Feat :: create rent service logic for book

--- src/main/java/com/keepgoing/keepserver/domain/rent/service/RentServiceImpl.java
+++ src/main/java/com/keepgoing/keepserver/domain/rent/service/RentServiceImpl.java
@@ -1,5 +1,9 @@
 package com.keepgoing.keepserver.domain.rent.service;
 
+import com.keepgoing.keepserver.domain.book.consts.BookState;
+import com.keepgoing.keepserver.domain.book.entity.Book;
+import com.keepgoing.keepserver.domain.book.mapper.BookMapper;
+import com.keepgoing.keepserver.domain.book.repository.BookRepository;
 import com.keepgoing.keepserver.domain.device.entity.Device;
 import com.keepgoing.keepserver.domain.device.entity.DeviceStatus;
 import com.keepgoing.keepserver.domain.device.mapper.DeviceMapper;
@@ -7,6 +11,8 @@ import com.keepgoing.keepserver.domain.device.repository.DeviceRepository;
 import com.keepgoing.keepserver.domain.device.service.DeviceServiceImpl;
 import com.keepgoing.keepserver.domain.user.entity.user.User;
 import com.keepgoing.keepserver.global.common.BaseResponse;
+import com.keepgoing.keepserver.global.exception.book.BookError;
+import com.keepgoing.keepserver.global.exception.book.BookException;
 import com.keepgoing.keepserver.global.exception.device.DeviceError;
 import com.keepgoing.keepserver.global.exception.device.DeviceException;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +25,8 @@ public class RentServiceImpl implements RentService{
     private final DeviceServiceImpl deviceService;
     private final DeviceRepository deviceRepository;
     private final DeviceMapper deviceMapper;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
     public BaseResponse rentDevice(String deviceName, String email) {
@@ -27,6 +35,15 @@ public class RentServiceImpl implements RentService{
         validateDeviceAvailability(device);
         rentDeviceToUser(device, user);
         return new BaseResponse(HttpStatus.OK, "기기 대여 성공", deviceMapper.entityToDto(device));
+    }
+
+    @Override
+    public BaseResponse rentBook(String bookName, String email) {
+        User user = deviceService.findUserByEmail(email);
+        Book book = findBookByName(bookName);
+        validateBookAvailability(book);
+        rentBookToUser(book, user);
+        return new BaseResponse(HttpStatus.OK, "도서 대여 성공", bookMapper.entityToDto(book));
     }
 
     private Device findDeviceByName(String deviceName) {
@@ -44,5 +61,22 @@ public class RentServiceImpl implements RentService{
         device.setBorrower(user);
         device.setStatus(DeviceStatus.RENTED);
         deviceRepository.save(device);
+    }
+
+    private Book findBookByName(String bookName) {
+        return bookRepository.findByBookName(bookName)
+                .orElseThrow(BookException::notFoundBook);
+    }
+
+    private void validateBookAvailability(Book book) {
+        if (book.getState() != BookState.AVAILABLE) {
+            throw new BookException(BookError.BOOK_NOT_AVAILABLE);
+        }
+    }
+
+    private void rentBookToUser(Book book, User user) {
+        book.setBorrower(user);
+        book.setState(BookState.RENTED);
+        bookRepository.save(book);
     }
 }
