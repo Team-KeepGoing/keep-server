456b165aa223ecaf38d582fc46f324b0a3006357
priverg163 <priverg163@dgsw.hs.kr>
2024-06-10 14:23:58 +0900

    Remove :: @Transactional

--- src/main/java/com/keepgoing/keepserver/domain/book/repository/BookRepository.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.keepgoing.keepserver.domain.book.repository;
 
 import com.keepgoing.keepserver.domain.book.entity.Book;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
