e06a9641e7ee41591a5743ab506c0509029e2003
priverg163 <priverg163@dgsw.hs.kr>
2024-06-10 14:15:10 +0900

    Add :: @Transactional

--- src/main/java/com/keepgoing/keepserver/domain/book/service/BookServiceImpl.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/service/BookServiceImpl.java
@@ -1,14 +1,18 @@
 package com.keepgoing.keepserver.domain.book.service;
 
+import com.keepgoing.keepserver.domain.book.consts.BookState;
 import com.keepgoing.keepserver.domain.book.entity.Book;
 import com.keepgoing.keepserver.domain.book.repository.BookRepository;
 import com.keepgoing.keepserver.domain.book.repository.dto.BookRequestDTO;
+import com.keepgoing.keepserver.global.common.S3.S3Uploader;
 import com.keepgoing.keepserver.global.util.GenerateCertCharacter;
 import com.keepgoing.keepserver.domain.user.repository.user.UserRepository;
 import com.keepgoing.keepserver.global.common.BaseResponse;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,25 +26,33 @@ import java.util.List;
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final UserRepository userRepository;
+    private final S3Uploader s3Uploader;
+    private final GenerateCertCharacter generateCertCharacter;
 
     @Override
     public BaseResponse bookRegister(Book book, MultipartFile multipartFile) {
+        try {
+            book.setImageUrl(s3Uploader.upload(multipartFile, "picture"));
+        } catch (IOException e) {
+            return new BaseResponse(HttpStatus.INTERNAL_SERVER_ERROR, "사진 업로드중 오류.");
+        }
         String nfcCode = createNfcCode();
         book.setRegistrationDate(new Date());
         book.setNfcCode(nfcCode);
-        book.setState("0");
+        book.setState(BookState.AVAILABLE);
+
+
         bookRepository.save(book);
         return new BaseResponse(HttpStatus.OK, "책 생성 성공");
     }
-
 
     @Override
     public BaseResponse selectAllBook() {
         return new BaseResponse(HttpStatus.OK, "책 불러오기 성공", (ArrayList<Book>) bookRepository.findAll());
     }
 
-
     @Override
+    @Transactional(rollbackOn = Exception.class)
     public BaseResponse deleteBook(String nfcCode, Authentication auth) {
         if (nfcCode == null || nfcCode.isEmpty()) {
             return new BaseResponse(HttpStatus.NOT_FOUND, "Invalid NFC code");
@@ -56,39 +68,33 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BaseResponse selectMyBook(Authentication auth) {
+        //noinspection OptionalGetWithoutIsPresent
         String user = userRepository.findByEmail(auth.getName()).get().getEmail();
         List<Book> books = bookRepository.findByNameContaining(user);
 
-        return new BaseResponse(HttpStatus.OK,"책 가져오기 성공", books.toString());
+        return new BaseResponse(HttpStatus.OK, "책 가져오기 성공", books.toString());
     }
 
     @Override
     public String createNfcCode() {
-        GenerateCertCharacter generateCertCharacter = new GenerateCertCharacter();
         String newNfcCode;
         do {
-            newNfcCode = generateCertCharacter.excuteGenerate();
+            newNfcCode = generateCertCharacter.executeGenerate();
         } while (bookRepository.findBookByNfcCode(newNfcCode) != null);
         return newNfcCode;
     }
 
     @Override
-    public BaseResponse editBook(String nfcCode, BookRequestDTO bookRequest) throws IOException {
+    @Transactional(rollbackOn = Exception.class)
+    public BaseResponse editBook(String nfcCode, BookRequestDTO bookRequest) {
         Book book = bookRepository.findBookByNfcCode(nfcCode);
 
-        List<BookRequestDTO> list = new ArrayList<>();
-        list.add(bookRequest);
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getState() != null) book.setState(list.get(i).getState());
-            if (list.get(i).getImageUrl() != null) book.setImageUrl(list.get(i).getImageUrl());
-            if (list.get(i).getName() != null) book.setName(list.get(i).getName());
-        }
+        if (bookRequest.getState() != null) book.setState(bookRequest.getState());
+        if (bookRequest.getImageUrl() != null) book.setImageUrl(bookRequest.getImageUrl());
+        if (bookRequest.getName() != null) book.setName(bookRequest.getName());
         bookRepository.save(book);
         return new BaseResponse(HttpStatus.OK, "책 정보 수정 성공");
     }
-
-
 }
 
 
