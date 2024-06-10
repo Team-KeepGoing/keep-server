3aa18d6711c4efbf6a898cc129eef733131a16de
최미래 <miraechoi@dgsw.hs.kr>
2024-06-10 14:47:52 +0900

    Feat :: create rent api

---
+++ src/main/java/com/keepgoing/keepserver/domain/rent/presentation/RentController.java
@@ -0,0 +1,22 @@
+package com.keepgoing.keepserver.domain.rent.presentation;
+
+import com.keepgoing.keepserver.domain.rent.service.RentService;
+import com.keepgoing.keepserver.global.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "대여", description = "대여 관련 api 입니다.")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/rent")
+@RequiredArgsConstructor
+public class RentController {
+    private final RentService rentService;
+    @Operation(summary = "기자재 대여", description = "기자재를 대여합니다.")
+    @PostMapping("/device")
+    public BaseResponse rentDevice(@RequestParam String deviceName, @RequestParam String email) {
+        return rentService.rentDevice(deviceName, email);
+    }
+}
