9da3277d3a3f3926c87b77589373ad78d2951b1a
최미래 <miraechoi@dgsw.hs.kr>
2024-06-11 14:26:53 +0900

    Add :: add convertBooksToDtos

--- src/main/java/com/keepgoing/keepserver/domain/book/mapper/BookMapper.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/mapper/BookMapper.java
@@ -4,14 +4,23 @@ import com.keepgoing.keepserver.domain.book.entity.Book;
 import com.keepgoing.keepserver.domain.book.entity.dto.BookResponseDto;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class BookMapper {
-    public static BookResponseDto entityToDto(Book entity) {
+    public BookResponseDto entityToDto(Book entity) {
         return BookResponseDto.builder()
                 .id(entity.getId())
                 .bookName(entity.getBookName())
                 .imageUrl(entity.getImageUrl())
                 .state(entity.getState())
                 .build();
+    }
+
+    public List<BookResponseDto> convertBooksToDtos(List<Book> books) {
+        return books.stream()
+                .map(this::entityToDto)
+                .collect(Collectors.toList());
     }
 }
