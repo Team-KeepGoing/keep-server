a8062b2ff10d6e0507793eaac7741bfc02820463
최미래 <miraechoi@dgsw.hs.kr>
2024-06-11 14:27:10 +0900

    Feat :: add findByBorrower

--- src/main/java/com/keepgoing/keepserver/domain/book/repository/BookRepository.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package com.keepgoing.keepserver.domain.book.repository;
 
 import com.keepgoing.keepserver.domain.book.entity.Book;
+import com.keepgoing.keepserver.domain.user.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,5 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookByNfcCode(String NfcCode);
     List<Book> findByBookNameContaining(String bookName);
     Optional<Book> findByBookName(String bookName);
+    List<Book> findByBorrower(User borrower);
 }
