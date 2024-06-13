ffb5f10c83c3ee31267660b02ccd0fbd8b6bace7
최미래 <miraechoi@dgsw.hs.kr>
2024-06-13 21:34:48 +0900

    Fix :: Date -> LocalDateTime

--- src/main/java/com/keepgoing/keepserver/domain/book/entity/dto/BookDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/entity/dto/BookDto.java
@@ -5,16 +5,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Getter
 @Setter
 public class BookDto {
     private long id;
     private String bookName;
+    private String writer;
     private String imageUrl;
     private String nfcCode;
-    private Date registrationDate;
+    private LocalDateTime registrationDate;
     private LocalDateTime rentDate;
     private BookState state;
 }
