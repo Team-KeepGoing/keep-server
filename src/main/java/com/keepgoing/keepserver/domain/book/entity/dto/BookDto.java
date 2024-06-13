961b6db2ff68589e40164e4a26d2d36fe9db490c
최미래 <miraechoi@dgsw.hs.kr>
2024-06-13 19:28:35 +0900

    Add :: add BookDto

--- src/main/java/com/keepgoing/keepserver/domain/book/entity/dto/BookDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/entity/dto/BookDto.java
@@ -3,10 +3,15 @@ package com.keepgoing.keepserver.domain.book.entity.dto;
 import com.keepgoing.keepserver.domain.book.consts.BookState;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Getter
 public class BookDto {
     private long id;
     private String bookName;
     private String imageUrl;
+    private Date registrationDate;
+    private LocalDateTime rentDate;
     private BookState state;
 }
