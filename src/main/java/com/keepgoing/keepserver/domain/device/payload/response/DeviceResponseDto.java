c81d3fead785ff7095cec8e48c52eaed4068fa34
최미래 <miraechoi@dgsw.hs.kr>
2024-06-13 17:12:35 +0900

    Refactor :: fix device response dto

--- src/main/java/com/keepgoing/keepserver/domain/device/payload/response/DeviceResponseDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/device/payload/response/DeviceResponseDto.java
@@ -1,13 +1,16 @@
 package com.keepgoing.keepserver.domain.device.payload.response;
 
-import com.keepgoing.keepserver.domain.device.entity.DeviceStatus;
+import com.keepgoing.keepserver.domain.device.enums.DeviceStatus;
 import lombok.Builder;
+
+import java.time.LocalDateTime;
 
 @Builder
 public record DeviceResponseDto (
         Long id,
         String deviceName,
         String imgUrl,
+        LocalDateTime rentDate,
         DeviceStatus status
 ) {
 }
