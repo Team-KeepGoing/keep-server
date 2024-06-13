f3c0c4f561d828feea649912ee55c2e2c220a642
최미래 <miraechoi@dgsw.hs.kr>
2024-06-13 20:00:50 +0900

    Add :: add nfcCode to book dto

--- src/main/java/com/keepgoing/keepserver/domain/book/entity/dto/BookDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/entity/dto/BookDto.java
@@ -2,15 +2,18 @@ package com.keepgoing.keepserver.domain.book.entity.dto;
 
 import com.keepgoing.keepserver.domain.book.consts.BookState;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
+@Setter
 public class BookDto {
     private long id;
     private String bookName;
     private String imageUrl;
+    private String nfcCode;
     private Date registrationDate;
     private LocalDateTime rentDate;
     private BookState state;
