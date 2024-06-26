85ef4640ffdcce16bea0747cef062d33fc9d3591
최미래 <miraechoi@dgsw.hs.kr>
2024-06-26 23:37:33 +0900

    feat :: device error code

--- src/main/java/com/keepgoing/keepserver/global/exception/device/DeviceError.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/device/DeviceError.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum DeviceError implements ErrorProperty {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
     DEVICE_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 기기를 찾을 수 없습니다."),
-    DEVICE_NOT_AVAILABLE(HttpStatus.BAD_REQUEST, "기기를 대여할 수 없습니다.");
+    DEVICE_NOT_AVAILABLE(HttpStatus.BAD_REQUEST, "기기를 대여할 수 없습니다."),
+    INVALID_BORROWER(HttpStatus.BAD_REQUEST, "반납 요청자가 기기의 대여자가 아닙니다.");
     private final HttpStatus status;
     private final String message;
 }
