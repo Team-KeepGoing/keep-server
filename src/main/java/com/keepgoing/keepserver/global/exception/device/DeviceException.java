ba8450254b0523395b2cbf1809432be3e21172fe
최미래 <miraechoi@dgsw.hs.kr>
2024-06-26 23:37:42 +0900

    feat :: device invalid brrower exception

--- src/main/java/com/keepgoing/keepserver/global/exception/device/DeviceException.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/device/DeviceException.java
@@ -1,12 +1,14 @@
 package com.keepgoing.keepserver.global.exception.device;
 
 import com.keepgoing.keepserver.global.exception.BusinessException;
+import com.keepgoing.keepserver.global.exception.book.BookException;
 
 public class DeviceException extends BusinessException {
     private static final DeviceException USER_NOT_FOUND = new DeviceException(DeviceError.USER_NOT_FOUND);
 
     private static final DeviceException DEVICE_NOT_FOUND_EXCEPTION = new DeviceException(DeviceError.DEVICE_NOT_FOUND_EXCEPTION);
     private static final DeviceException DEVICE_NOT_AVAILABLE = new DeviceException(DeviceError.DEVICE_NOT_AVAILABLE);
+    private static final DeviceException INVALID_BORROWER = new DeviceException(DeviceError.INVALID_BORROWER);
 
     public DeviceException(DeviceError error) {
         super(error);
@@ -21,5 +23,8 @@ public class DeviceException extends BusinessException {
     }
     public static DeviceException deviceNotAvailable() {
         return DEVICE_NOT_AVAILABLE;
+    }
+    public static DeviceException invalidborrower() {
+        return INVALID_BORROWER;
     }
 }
