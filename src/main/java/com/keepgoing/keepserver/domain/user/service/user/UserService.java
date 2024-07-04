01ba0952283b6c225743b2b088eb74f9c05dffa0
최미래 <miraechoi@dgsw.hs.kr>
2024-07-04 21:04:28 +0900

    refactor :: add user service

--- src/main/java/com/keepgoing/keepserver/domain/user/service/user/UserService.java
+++ src/main/java/com/keepgoing/keepserver/domain/user/service/user/UserService.java
@@ -3,10 +3,12 @@ package com.keepgoing.keepserver.domain.user.service.user;
 import com.keepgoing.keepserver.domain.user.payload.request.SignupRequest;
 import com.keepgoing.keepserver.domain.user.payload.request.UserInfoRequest;
 import com.keepgoing.keepserver.domain.user.payload.request.UserProfileDto;
+import com.keepgoing.keepserver.domain.user.payload.response.JwtResponse;
 import com.keepgoing.keepserver.global.exception.BusinessException;
 
 public interface UserService {
     void registerUser(SignupRequest signupRequest) throws BusinessException;
     void updateUserData(UserInfoRequest request, String email);
     UserProfileDto provideUserInfo(String userEmail);
+    JwtResponse authenticateAndGenerateJWT(String email, String password);
 }
