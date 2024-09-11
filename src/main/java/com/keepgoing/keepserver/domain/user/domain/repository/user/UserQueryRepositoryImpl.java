d97292fb5b4574a94c3885fe83d53de1dac339c5
priverg163 <priverg163@dgsw.hs.kr>
2024-09-11 23:47:37 +0900

    feat :: notice object in user dto

--- src/main/java/com/keepgoing/keepserver/domain/user/domain/repository/user/UserQueryRepositoryImpl.java
+++ src/main/java/com/keepgoing/keepserver/domain/user/domain/repository/user/UserQueryRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.keepgoing.keepserver.domain.user.domain.repository.user;
 
 import com.keepgoing.keepserver.domain.book.payload.response.QBookResponseDto;
 import com.keepgoing.keepserver.domain.device.payload.response.QDeviceResponseDto;
+import com.keepgoing.keepserver.domain.notice.payload.res.QNoticeResponseDto;
+import com.keepgoing.keepserver.domain.user.domain.entity.user.QUser;
 import com.keepgoing.keepserver.domain.user.dto.QUserDto;
 import com.keepgoing.keepserver.domain.user.dto.UserDto;
 import com.querydsl.core.group.GroupBy;
@@ -14,16 +16,22 @@ import static com.keepgoing.keepserver.domain.book.domain.entity.QBook.book;
 import static com.keepgoing.keepserver.domain.device.domain.entity.QDevice.device;
 import static com.keepgoing.keepserver.domain.notice.domain.entity.notice.QNoticeReception.noticeReception;
 import static com.keepgoing.keepserver.domain.user.domain.entity.user.QUser.user;
+import static com.keepgoing.keepserver.domain.notice.domain.entity.notice.QNotice.notice;
 
 @RequiredArgsConstructor
 public class UserQueryRepositoryImpl implements UserQueryRepository {
+    // teacher 별칭을 따로 설정
     private final JPAQueryFactory factory;
 
     @Override
     public UserDto getProfileByEmail(long id) {
+        var teacher = new QUser("teacher");
         var map = factory
-                .selectFrom(user)
-                .leftJoin(noticeReception).on(noticeReception.userId.id.eq(user.id))
+                .select(user)
+                .from(user)
+                .leftJoin(noticeReception).on(noticeReception.user.id.eq(user.id))
+                .leftJoin(noticeReception.notice, notice)
+                .leftJoin(notice.teacher, teacher)
                 .leftJoin(device).on(device.borrower.id.eq(user.id))
                 .leftJoin(book).on(book.borrower.id.eq(user.id))
                 .where(user.id.eq(id))
@@ -54,10 +62,20 @@ public class UserQueryRepositoryImpl implements UserQueryRepository {
                                                 book.rentDate,
                                                 book.state
                                         )
+                                ),
+                                GroupBy.set(
+                                        new QNoticeResponseDto(
+                                                notice.idx,
+                                                notice.message,
+                                                teacher.name,
+                                                notice.createTime,
+                                                notice.editTime
+                                        )
                                 )
                         ))
                 );
 
+        System.out.println(map);
         var dto = map.get(id);
         return UserDto.builder()
                 .id(dto.id())
@@ -66,6 +84,7 @@ public class UserQueryRepositoryImpl implements UserQueryRepository {
                 .teacher(dto.teacher())
                 .borrowedDevices(dto.borrowedDevices().stream().filter(r -> r.id() != 0).collect(Collectors.toSet()))
                 .borrowedBooks(dto.borrowedBooks().stream().filter(r -> r.id() != 0).collect(Collectors.toSet()))
+                .notices(dto.notices().stream().filter(r -> r.id() != 0).collect(Collectors.toSet()))
                 .build();
     }
 }
--- src/main/java/com/keepgoing/keepserver/domain/user/dto/UserDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/user/dto/UserDto.java
@@ -2,14 +2,22 @@ package com.keepgoing.keepserver.domain.user.dto;
 
 import com.keepgoing.keepserver.domain.book.payload.response.BookResponseDto;
 import com.keepgoing.keepserver.domain.device.payload.response.DeviceResponseDto;
+import com.keepgoing.keepserver.domain.notice.payload.res.NoticeResponseDto;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 
 import java.util.Set;
 
 @Builder
-public record UserDto(Long id, String email, String name, boolean teacher, Set<DeviceResponseDto> borrowedDevices, Set<BookResponseDto> borrowedBooks) {
-    //    private List<Notice> notices;
+public record UserDto(
+        Long id,
+        String email,
+        String name,
+        boolean teacher,
+        Set<DeviceResponseDto> borrowedDevices,
+        Set<BookResponseDto> borrowedBooks,
+        Set<NoticeResponseDto> notices
+) {
 
     @QueryProjection
     public UserDto {}
