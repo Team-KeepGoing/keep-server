a03e23571a89e94d470a7464f064568ba997b611
priverg163 <priverg163@dgsw.hs.kr>
2024-09-18 12:10:15 +0900

    Add :: handleValidationExceptions

--- src/main/java/com/keepgoing/keepserver/global/exception/ExceptionAdvice.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/ExceptionAdvice.java
@@ -1,9 +1,15 @@
 package com.keepgoing.keepserver.global.exception;
 
 import lombok.Builder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
@@ -19,5 +25,14 @@ public class ExceptionAdvice {
                 .message(ex.getError().getMessage())
                 .build();
         return new ResponseEntity<>(response, ex.getError().getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
