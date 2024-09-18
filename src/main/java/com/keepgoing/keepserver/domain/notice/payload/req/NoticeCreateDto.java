eb6278a34a3143d251865ee0e60c87faf0d69e13
priverg163 <priverg163@dgsw.hs.kr>
2024-09-18 12:09:46 +0900

    Add :: @Valid in NoticeController

--- src/main/java/com/keepgoing/keepserver/domain/notice/payload/req/NoticeCreateDto.java
+++ src/main/java/com/keepgoing/keepserver/domain/notice/payload/req/NoticeCreateDto.java
@@ -1,12 +1,16 @@
 package com.keepgoing.keepserver.domain.notice.payload.req;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record NoticeCreateDto (
+        @NotBlank(message = "메시지는 필수입니다.")
         String message,
+        @NotNull(message = "isGlobal 값은 필수입니다.")
         boolean isGlobal,
         List<Long> userIds
 ) {
--- src/main/java/com/keepgoing/keepserver/domain/notice/presentation/NoticeController.java
+++ src/main/java/com/keepgoing/keepserver/domain/notice/presentation/NoticeController.java
@@ -5,6 +5,7 @@ import com.keepgoing.keepserver.domain.notice.service.NoticeService;
 import com.keepgoing.keepserver.global.common.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class NoticeController {
     private final NoticeService noticeService;
     @Operation(summary = "공지 등록", description = "공지 등록을 진행합니다. 전체 학생을 등록하고 싶으실 경우엔 isGlobal을 true로 해주시고, list는 빈값으로 보내시면 됩니다.")
     @PostMapping("/post")
-    public BaseResponse addNotice(@RequestBody NoticeCreateDto noticeCreateDto, Authentication authentication) {
+    public BaseResponse addNotice(@Valid @RequestBody NoticeCreateDto noticeCreateDto, Authentication authentication) {
         return noticeService.createNotice(noticeCreateDto,authentication);
     }
     @Operation(summary = "공지 수정", description = "등록된 공지를 수정합니다.")
