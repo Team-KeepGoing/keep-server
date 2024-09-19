a1c655dcb64a168a47b01a3d0a7564ce14815e2b
priverg163 <priverg163@dgsw.hs.kr>
2024-09-19 17:26:36 +0900

    Refactor :: remove unused errorCode in NoticeException

--- src/main/java/com/keepgoing/keepserver/global/exception/notice/NoticeError.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/notice/NoticeError.java
@@ -8,9 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum NoticeError implements ErrorProperty {
-    USER_NOT_TEACHER(HttpStatus.BAD_REQUEST, "선생님이 아닙니다."),
     USER_CANNOT_DELETE(HttpStatus.BAD_REQUEST, "본인 글이 아닙니다.");
-
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/keepgoing/keepserver/global/exception/notice/NoticeException.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/notice/NoticeException.java
@@ -4,16 +4,10 @@ package com.keepgoing.keepserver.global.exception.notice;
 import com.keepgoing.keepserver.global.exception.BusinessException;
 
 public class NoticeException extends BusinessException {
-    private static final NoticeException USER_NOT_TEACHER = new NoticeException(NoticeError.USER_NOT_TEACHER);
     private static final NoticeException USER_CANNOT_DELETE = new NoticeException(NoticeError.USER_CANNOT_DELETE);
-
 
     public NoticeException(NoticeError error) {
         super(error);
-    }
-
-    public static NoticeException userNotTeacher(){
-        return USER_NOT_TEACHER;
     }
 
     public static NoticeException userCannotDelete(){
