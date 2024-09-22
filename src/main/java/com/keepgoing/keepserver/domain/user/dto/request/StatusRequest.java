e379cf05bf56181e964cf15f1e7f6e5ca838e360
priverg163 <priverg163@dgsw.hs.kr>
2024-09-22 22:09:19 +0900

    Add :: NotBlank annotation in StatusRequest

--- src/main/java/com/keepgoing/keepserver/domain/user/dto/request/StatusRequest.java
+++ src/main/java/com/keepgoing/keepserver/domain/user/dto/request/StatusRequest.java
@@ -1,8 +1,10 @@
 package com.keepgoing.keepserver.domain.user.dto.request;
 
 import com.keepgoing.keepserver.domain.user.domain.enums.Status;
+import jakarta.validation.constraints.NotBlank;
 
 public record StatusRequest (
+        @NotBlank(message = "Status 값은 필수입니다. (NORMAL/PAIN/HEALTHROOM/EMERGENCY")
         Status status
 ) {
 }
