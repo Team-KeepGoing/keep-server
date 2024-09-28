916d8d2fdbff47ef56d72994cc5c469c55a83f29
priverg163 <priverg163@dgsw.hs.kr>
2024-09-28 23:57:52 +0900

    Refactor :: unused Code in Book Mapper

--- src/main/java/com/keepgoing/keepserver/domain/book/mapper/BookMapper.java
+++ src/main/java/com/keepgoing/keepserver/domain/book/mapper/BookMapper.java
@@ -7,9 +7,6 @@ import com.keepgoing.keepserver.domain.book.payload.response.BookResponseDto;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 public class BookMapper {
     public BookResponseDto entityToDto(Book entity) {
@@ -34,11 +31,5 @@ public class BookMapper {
                 .state(BookState.AVAILABLE)
                 .nfcCode(nfcCode)
                 .build();
-    }
-
-    public List<BookResponseDto> convertBooksToDtos(List<Book> books) {
-        return books.stream()
-                .map(this::entityToDto)
-                .collect(Collectors.toList());
     }
 }
