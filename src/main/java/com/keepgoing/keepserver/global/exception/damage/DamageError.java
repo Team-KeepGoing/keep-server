1aebc35219c54684aec76ddc93490dde16706c6f
최미래 <miraechoi@dgsw.hs.kr>
2024-10-15 20:37:26 +0900

    feature :: add invalid issue type error

--- src/main/java/com/keepgoing/keepserver/global/exception/damage/DamageError.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/damage/DamageError.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum DamageError implements ErrorProperty {
     INVALID_DAMAGE(HttpStatus.NOT_FOUND, "파손 정보가 없습니다."),
-    DUPLICATE(HttpStatus.NOT_FOUND, "이미 같은 문제로 신고된 기기입니다.");
+    DUPLICATE(HttpStatus.BAD_REQUEST, "이미 같은 문제로 신고된 기기입니다."),
+    INVALID_ISSUE_TYPE(HttpStatus.BAD_REQUEST, "유효한 신고 유형을 선택해 주세요.");
     private final HttpStatus status;
     private final String message;
 }
