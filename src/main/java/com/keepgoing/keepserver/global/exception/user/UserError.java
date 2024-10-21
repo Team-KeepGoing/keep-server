7afdcc6fbacee40f1fd873543b82d0f99018d075
최미래 <miraechoi@dgsw.hs.kr>
2024-10-21 21:13:16 +0900

    feature :: add user error code

--- src/main/java/com/keepgoing/keepserver/global/exception/user/UserError.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/user/UserError.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserError implements ErrorProperty {
     USER_NOT_TEACHER(HttpStatus.BAD_REQUEST, "선생님이 아닙니다."),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 학생을 찾을 수 없습니다");
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 학생을 찾을 수 없습니다"),
+    TEACHER_ACCOUNT_NOT_APPROVED(HttpStatus.UNAUTHORIZED, "교사 계정이 승인되지 않았습니다.");
 
     private final HttpStatus status;
     private final String message;
