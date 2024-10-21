fa1be7a5cd1dd19dfa1120c084b86cbbd738a147
최미래 <miraechoi@dgsw.hs.kr>
2024-10-21 21:13:20 +0900

    feature :: add user error exception

--- src/main/java/com/keepgoing/keepserver/global/exception/user/UserException.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/user/UserException.java
@@ -6,6 +6,7 @@ import com.keepgoing.keepserver.global.exception.error.ErrorProperty;
 public class UserException extends BusinessException {
     private static final UserException USER_NOT_TEACHER = new UserException(UserError.USER_NOT_TEACHER);
     private static final UserException USER_NOT_FOUND = new UserException(UserError.USER_NOT_FOUND);
+    private static final UserException TEACHER_ACCOUNT_NOT_APPROVED = new UserException(UserError.TEACHER_ACCOUNT_NOT_APPROVED);
 
     public UserException(ErrorProperty error) {
         super(error);
@@ -15,4 +16,5 @@ public class UserException extends BusinessException {
         return USER_NOT_TEACHER;
     }
     public static UserException userNotFound(){ return USER_NOT_FOUND; }
+    public static UserException teacherAccountNotApproved() {return TEACHER_ACCOUNT_NOT_APPROVED; }
 }
