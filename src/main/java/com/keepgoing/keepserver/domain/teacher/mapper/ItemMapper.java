34c1b484c1af3191ec74e5c066ab4da53e581e76
최미래 <miraechoi@dgsw.hs.kr>
2025-04-02 17:58:20 +0900

    feature: add item mapper

---
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/mapper/ItemMapper.java
@@ -0,0 +1,31 @@
+package com.keepgoing.keepserver.domain.teacher.mapper;
+
+import com.keepgoing.keepserver.domain.teacher.domain.entity.Item;
+import com.keepgoing.keepserver.domain.teacher.payload.response.ItemResponseDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ItemMapper {
+
+    public ItemResponseDto entityToDto(Item entity) {
+        return ItemResponseDto.builder()
+                .id(entity.getId())
+                .item(entity.getItem())
+                .serial_number(entity.getSerial_number())
+                .acquisition_date(entity.getAcquisition_date())
+                .price(entity.getPrice())
+                .register_person(entity.getRegister_person())
+                .usage_date(entity.getUsage_date())
+                .memo(entity.getMemo())
+                .build();
+    }
+
+    public List<ItemResponseDto> convertItemsToDtos(List<Item> items) {
+        return items.stream()
+                .map(this::entityToDto)
+                .collect(Collectors.toList());
+    }
+}
