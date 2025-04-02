e824357ac30127f7723df4e4808c42c8fb0dbc0f
최미래 <miraechoi@dgsw.hs.kr>
2025-04-02 17:43:27 +0900

    feature: add item entity for teacher

---
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/domain/entity/Item.java
@@ -0,0 +1,63 @@
+package com.keepgoing.keepserver.domain.teacher.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "manage")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /*
+        품명
+    */
+    @Column(nullable = false)
+    private String item;
+
+    /*
+        분류 번호
+    */
+    @Column(nullable = false)
+    private String serial_number;
+
+    /*
+        취득 일자
+    */
+    @Column(nullable = false)
+    private LocalDateTime acquisition_date;
+
+    /*
+        취득 단가
+    */
+    @Column(nullable = false)
+    private LocalDateTime price;
+
+    /*
+        등록자
+    */
+    @Column(nullable = false)
+    private String register_person;
+
+    /*
+        사용 일수
+    */
+    @Column(nullable = true)
+    private LocalDateTime usage_date;
+
+    /*
+        메모
+    */
+    @Column(nullable = true)
+    private LocalDateTime memo;
+
+}
