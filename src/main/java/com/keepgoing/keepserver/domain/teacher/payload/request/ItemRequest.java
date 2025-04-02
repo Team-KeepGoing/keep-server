26b0bedbf8caea5b82f2c4781c34ed83cb9dd47b
최미래 <miraechoi@dgsw.hs.kr>
2025-04-02 19:32:17 +0900

    feature: add item request dto

---
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/payload/request/ItemRequest.java
@@ -0,0 +1,12 @@
+package com.keepgoing.keepserver.domain.teacher.payload.request;
+
+import java.time.LocalDateTime;
+
+public record ItemRequest (
+        String item,
+        String serial_number,
+        LocalDateTime acquisition_date,
+        LocalDateTime price,
+        String register_person
+) {
+}
