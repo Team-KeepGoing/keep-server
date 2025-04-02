5c455b919d65c4924e9249209698266d270a6daa
최미래 <miraechoi@dgsw.hs.kr>
2025-04-02 18:00:32 +0900

    feature: add item findAll list api

---
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/presentation/TeacherController.java
@@ -0,0 +1,25 @@
+package com.keepgoing.keepserver.domain.teacher.presentation;
+
+import com.keepgoing.keepserver.domain.teacher.service.ItemService;
+import com.keepgoing.keepserver.global.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "교사용", description = "교사용 관리 api 입니다.")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/teacher")
+@RequiredArgsConstructor
+public class TeacherController {
+
+    private final ItemService itemService;
+
+    @Operation(summary = "관리 물품 리스트 조회", description = "관리 중인 물품 리스트를 조회합니다.")
+    @GetMapping("/item/list")
+    public BaseResponse allItems(){
+        return itemService.findAll();
+    }
+
+}
