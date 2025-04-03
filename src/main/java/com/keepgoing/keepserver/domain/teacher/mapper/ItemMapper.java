6d0fe76cda9a5065cded8ab726d12c9f830e9dd7
최미래 <miraechoi@dgsw.hs.kr>
2025-04-03 14:19:01 +0900

    refactor: apply mapstruct

--- src/main/java/com/keepgoing/keepserver/domain/teacher/mapper/ItemMapper.java
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/mapper/ItemMapper.java
@@ -3,40 +3,31 @@ package com.keepgoing.keepserver.domain.teacher.mapper;
 import com.keepgoing.keepserver.domain.teacher.domain.entity.Item;
 import com.keepgoing.keepserver.domain.teacher.payload.request.ItemRequest;
 import com.keepgoing.keepserver.domain.teacher.payload.response.ItemResponse;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
-public class ItemMapper {
+@Mapper(componentModel = "spring")
+public interface ItemMapper {
 
-    public ItemResponse entityToDto(Item entity) {
-        return ItemResponse.builder()
-                .id(entity.getId())
-                .item(entity.getItem())
-                .serialNumber(entity.getSerialNumber())
-                .acquisitionDate(entity.getAcquisitionDate())
-                .price(entity.getPrice())
-                .registerPerson(entity.getRegisterPerson())
-                .usageDate(entity.getUsageDate())
-                .memo(entity.getMemo())
-                .build();
-    }
+    @Mapping(source = "entity.id", target = "id")
+    @Mapping(source = "entity.item", target = "item")
+    @Mapping(source = "entity.serialNumber", target = "serialNumber")
+    @Mapping(source = "entity.acquisitionDate", target = "acquisitionDate")
+    @Mapping(source = "entity.price", target = "price")
+    @Mapping(source = "entity.registerPerson", target = "registerPerson")
+    @Mapping(source = "entity.usageDate", target = "usageDate")
+    @Mapping(source = "entity.memo", target = "memo")
+    ItemResponse entityToDto(Item entity);
 
-    public Item dtoToEntity(ItemRequest dto) {
-        return Item.builder()
-                .item(dto.item())
-                .serialNumber(dto.serialNumber())
-                .acquisitionDate(dto.acquisitionDate())
-                .price(dto.price())
-                .registerPerson(dto.registerPerson())
-                .build();
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "dto.item", target = "item")
+    @Mapping(source = "dto.serialNumber", target = "serialNumber")
+    @Mapping(source = "dto.acquisitionDate", target = "acquisitionDate")
+    @Mapping(source = "dto.price", target = "price")
+    @Mapping(source = "dto.registerPerson", target = "registerPerson")
+    Item dtoToEntity(ItemRequest dto);
 
-    public List<ItemResponse> convertItemsToDtos(List<Item> items) {
-        return items.stream()
-                .map(this::entityToDto)
-                .collect(Collectors.toList());
-    }
+    List<ItemResponse> convertItemsToDtos(List<Item> items);
 }
