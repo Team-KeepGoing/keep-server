db7890a2536c9320794915e30e459552c56fcda0
최미래 <miraechoi@dgsw.hs.kr>
2025-04-03 13:49:37 +0900

    feature: add count by status to repository

--- src/main/java/com/keepgoing/keepserver/domain/teacher/domain/repository/ItemRepository.java
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/domain/repository/ItemRepository.java
@@ -1,7 +1,9 @@
 package com.keepgoing.keepserver.domain.teacher.domain.repository;
 
 import com.keepgoing.keepserver.domain.teacher.domain.entity.Item;
+import com.keepgoing.keepserver.domain.teacher.domain.entity.enums.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Long countByStatus(ItemStatus status);
 }
