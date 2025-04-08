a704ce21890af10196f8c76095042a245fd8f3cd
최미래 <miraechoi@dgsw.hs.kr>
2025-04-08 12:17:35 +0900

    refactor: remove unused code

--- src/main/java/com/keepgoing/keepserver/domain/teacher/payload/request/ItemUpdateRequest.java
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/payload/request/ItemUpdateRequest.java
@@ -1,11 +1,5 @@
 package com.keepgoing.keepserver.domain.teacher.payload.request;
 
-import com.keepgoing.keepserver.domain.teacher.domain.entity.enums.ItemStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 
 public record ItemUpdateRequest(
@@ -18,7 +12,6 @@ public record ItemUpdateRequest(
         String place,
         LocalDateTime returnDate,
         LocalDateTime rentalDate,
-        LocalDateTime usageDate,
-        ItemStatus status
+        LocalDateTime usageDate
 ) {}
 
