a6e11dfff723109a077beac5f0ff275c117f9eb8
priverg163 <priverg163@dgsw.hs.kr>
2025-04-16 20:19:06 +0900

    refactor :: add excel error and factory pattern

--- src/main/java/com/keepgoing/keepserver/global/exception/excel/ExcelException.java
+++ src/main/java/com/keepgoing/keepserver/global/exception/excel/ExcelException.java
@@ -5,11 +5,41 @@ import com.keepgoing.keepserver.global.exception.BusinessException;
 public class ExcelException extends BusinessException {
     public static final ExcelException EXCEL_NOT_AVAILABLE = new ExcelException(ExcelError.EXCEL_NOT_AVAILABLE);
     public static final ExcelException EXCEL_UNSUPPORTED_TYPE = new ExcelException(ExcelError.EXCEL_UNSUPPORTED_TYPE);
-    public static final ExcelException UNABLE_TO_INSTANTIATE = new ExcelException(ExcelError.UNABLE_TO_INSTANTIATE);
+    public static final ExcelException EXCEL_NOT_CREATE = new ExcelException(ExcelError.EXCEL_NOT_CREATE);
     public static final ExcelException EXCEL_DATE_FORMAT_ERROR = new ExcelException(ExcelError.EXCEL_DATE_FORMAT_ERROR);
+    public static final ExcelException EXCEL_STATUS_VALUE_ERROR = new ExcelException(ExcelError.EXCEL_STATUS_VALUE_ERROR);
+    public static final ExcelException EXCEL_CANNOT_PARSE = new ExcelException(ExcelError.EXCEL_CANNOT_PARSE);
     public static final ExcelException EXCEL_VALIDATION_ERROR = new ExcelException(ExcelError.EXCEL_VALIDATION_ERROR);
 
     public ExcelException(ExcelError error) {
         super(error);
+    }
+
+    public static ExcelException excelNotAvailable() {
+        return EXCEL_NOT_AVAILABLE;
+    }
+
+    public static ExcelException excelUnsupportedType() {
+        return EXCEL_UNSUPPORTED_TYPE;
+    }
+
+    public static ExcelException excelNotCreate() {
+        return EXCEL_NOT_CREATE;
+    }
+
+    public static ExcelException excelDateFormatError() {
+        return EXCEL_DATE_FORMAT_ERROR;
+    }
+
+    public static ExcelException excelStatusValueError() {
+        return EXCEL_STATUS_VALUE_ERROR;
+    }
+
+    public static ExcelException excelCannotParse() {
+        return EXCEL_CANNOT_PARSE;
+    }
+
+    public static ExcelException excelValidationError(){
+        return EXCEL_VALIDATION_ERROR;
     }
 }
