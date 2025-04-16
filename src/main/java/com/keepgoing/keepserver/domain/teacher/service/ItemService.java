79d6a45334df5c405b2731e60cf8dd8a7ec864d5
priverg163 <priverg163@dgsw.hs.kr>
2025-04-17 08:37:31 +0900

    refactor :: remove unused import statement

--- src/main/java/com/keepgoing/keepserver/domain/teacher/service/ItemService.java
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/service/ItemService.java
@@ -5,7 +5,6 @@ import com.keepgoing.keepserver.domain.teacher.payload.request.ItemStatusUpdateR
 import com.keepgoing.keepserver.domain.teacher.payload.request.ItemUpdateRequest;
 import com.keepgoing.keepserver.global.common.BaseResponse;
 import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ItemService {
