4a3031d230f173e9051adb86901df8547bf26fca
priverg163 <priverg163@dgsw.hs.kr>
2025-04-16 22:55:58 +0900

    refactor :: unused import

--- src/main/java/com/keepgoing/keepserver/domain/teacher/service/ItemServiceImpl.java
+++ src/main/java/com/keepgoing/keepserver/domain/teacher/service/ItemServiceImpl.java
@@ -29,7 +29,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
